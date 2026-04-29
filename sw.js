ebbd703fc5b0138a8179c8e419e4c0599bb8b847
cool-running <jinnykim61@gmail.com>
2026-04-29 12:59:54 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,15 +1,16 @@
-const CACHE = 'cover-card-v3';
-const FILES = [
+const CACHE = 'sonhae-v1';
+const ASSETS = [
+  './',
   './index.html',
   './cover_card.html',
   './manifest.json',
   './icon-192.png',
-  './icon-512.png'
+  './icon-512.png',
 ];
 
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting())
+    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
   );
 });
 
@@ -21,15 +22,8 @@ self.addEventListener('activate', e => {
   );
 });
 
-/* 항상 네트워크 우선 → 캐시 폴백 */
 self.addEventListener('fetch', e => {
   e.respondWith(
-    fetch(e.request)
-      .then(res => {
-        const clone = res.clone();
-        caches.open(CACHE).then(c => c.put(e.request, clone));
-        return res;
-      })
-      .catch(() => caches.match(e.request))
+    caches.match(e.request).then(r => r || fetch(e.request))
   );
 });
