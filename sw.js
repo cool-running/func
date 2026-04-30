7afbbe17dbfca4cc5f76f38a610c542014897b7e
cool-running <jinnykim61@gmail.com>
2026-04-30 16:17:03 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,33 +1,5 @@
-const CACHE = 'sonhae-202604300705';
-const ASSETS = [
-  './',
-  './index.html',
-  './cover_card.html',
-  './camera.html',
-  './location.html',
-  './area.html',
-  './settings.html',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png',
-];
-
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
-  );
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    ).then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
-});
+const CACHE = 'sonhae-202604300715';
+const ASSETS = ['./', './index.html', './cover_card.html', './camera.html', './location.html', './area.html', './settings.html', './manifest.json', './icon-192.png', './icon-512.png'];
+self.addEventListener('install', e=>{ e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())); });
+self.addEventListener('activate', e=>{ e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())); });
+self.addEventListener('fetch', e=>{ e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))); });
