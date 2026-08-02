eaa7ea18df18705f00fd4e075bd3d84c345d0067
cool-running <jinnykim61@gmail.com>
2026-08-02 12:07:44 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,9 @@
-const CACHE = 'sonhae-202608021900';
+const CACHE = 'sonhae-202608022100';
 const ASSETS = ['./', './index.html', './cover_card.html', './camera.html', './location.html', './area.html', './settings.html', './manifest.json', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', e=>{ e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())); });
 self.addEventListener('activate', e=>{ e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())); });
-self.addEventListener('fetch', e=>{ e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))); });
+self.addEventListener('fetch', e=>{
+  const url = new URL(e.request.url);
+  if(url.origin !== self.location.origin) return; // 외부(카카오 SDK, vworld, 구글 폰트 등)는 그대로 네트워크로
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+});
